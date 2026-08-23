ddb68ec10b247fc2a7a040edc4d7e24a07cd3869
jorgesgovia <cuasi_jorge@hotmail.com>
2026-08-23 11:15:39 -0600

    Use bundled yt-dlp with execa for Render

--- src/streams.js
+++ src/streams.js
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import ytdlp from "yt-dlp-exec";
+import { execa } from "execa";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
